c166911662d5a06a932922355b90188741be84d9
Malcolm Foster <malcolmfoster@Malcolms-MacBook-Air.local>
2018-03-04 09:48:47 -0700

    added cors headers

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 const queries = require("./queries");
 const bodyParser = require("body-parser");
 const database = require("./database-connection")
 
+app.use(cors())
 app.use(bodyParser.json());
 
 app.get("/article", (request, response) => {
